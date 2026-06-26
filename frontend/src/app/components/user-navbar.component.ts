c064ecd3783f19b1721f1cb71c72f8032dc119a8
Alejandro Fernando Frias Torres <afriast@dcpteknology.com>
2026-06-25 21:23:48 -0600

    deploy: version PROD deployed to SiteGround

--- frontend/src/app/components/user-navbar.component.ts
+++ frontend/src/app/components/user-navbar.component.ts
@@ -15,11 +15,11 @@ import Swal from 'sweetalert2';
       <nav class="top-nav">
         <div class="top-nav-content">
           <div class="left-links">
-            <a (click)="logout()" class="nav-link cursor-pointer">Salir</a>
-            <span class="separator">|</span>
             <a routerLink="/rewards" class="nav-link">Inicio</a>
             <span class="separator">|</span>
             <a routerLink="/history" class="nav-link">Historial</a>
+            <span class="separator">|</span>
+            <a (click)="logout()" class="nav-link cursor-pointer">Salir</a>
           </div>
           <div class="right-info">
             <span class="info-item">
